f181b998ad406bc42178c4c3704169befff19859
Colin Millar <colin.millar@ices.dk>
2020-10-22 15:19:49 +0200

    table 3 legend

--- shiny_ui.R
+++ shiny_ui.R
@@ -130,7 +130,7 @@ allocations_resultspanel_wide <-
     column(12,
       conditionalPanel("output.hide_panel",
         wellPanel(
-          h5(helpText("Table 3: Forecast scenarios. Comparison between the simulated scenario (highlighted row) and the basis of ICES advice for 2020. Note that the advice % change value is relative to the advice value last year, MSY lower (1634 tonnes) and MSY upper (1946 tonnes), is relative to the corresponding scenario.")),
+          h5(helpText("Table 3: Forecast scenarios. Comparison between the simulated scenario (highlighted row) and the basis of ICES advice for 2020. Weights are in tonnes. Note that the '% Advice change' value is relative to the advice for the corresponding scenario for 2020: MSY lower (1634 tonnes) and MSY upper (1946 tonnes).")),
           br(),
           DT::dataTableOutput("forecastTable")
         )
